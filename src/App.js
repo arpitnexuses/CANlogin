e63761140ea32db04f52bf8ee9043fd09ae2ef9e
arpit-ma <86701463+arpit-ma@users.noreply.github.com>
2022-09-08 11:55:17 +0530

    CAN1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import Login from './Auth0/Login';
 import Blank from './component/Blank'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const {isAuthenticated} = useAuth0();
   return (
